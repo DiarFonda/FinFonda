cf6b567290ba22a7b5a6543d40e9d17cf07e4f02
Diar Fonda <Diarfondaa@gmail.com>
2025-03-19 02:53:36 +0100

    setup backend dir < entity framework

---
+++ api/Program.cs
@@ -0,0 +1,49 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add OpenAPI (Swagger) support
+builder.Services.AddEndpointsApiExplorer();  // Required for the OpenAPI documentation
+builder.Services.AddSwaggerGen(); // Adds Swagger services for API documentation
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.useSqlServer(builder.Configuration.GetConnectionString('DefaulConnection'))
+})
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Enable Swagger UI
+    // app.UseSwaggerUI(); // Display the Swagger UI in Development mode
+}
+
+app.UseHttpsRedirection(); // Redirect HTTP to HTTPS
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ api/models/Stock.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.models
+{
+    public class Stock
+    {
+        public int id { get; set; }
+        public string Symbol { get; set; }
+        public string CompanyName { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+        public decimal lastDiv { get; set; }
+        public string Industry { get; set; }
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>
+    }
+}
